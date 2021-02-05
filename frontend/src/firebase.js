3144fb97f37abcb244577ce50f1bbca039f1531f
Akshit Goyal <goyalakshit.ag@gmail.com>
2021-02-04 19:57:47 -0500

    [CHED-20] Temporary change to firebase setup using actual keys rather than local env

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,16 +1,27 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
+// Note: Uncomment the following and delete actual keys when everyone has setup firebase locally in .env.local file
 
  // Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID
+//   };
+
   var firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyCxqOMgfEntJZQMt3TFG1vO_6ljxTwvZaM",
+    authDomain: "cheddar-dev-cfcbf.firebaseapp.com",
+    projectId: "cheddar-dev-cfcbf",
+    storageBucket: "cheddar-dev-cfcbf.appspot.com",
+    messagingSenderId: "977144514707",
+    appId: "1:977144514707:web:4cfb35590b8c8ddd8e2566"
   };
+
 
   const app = firebase.initializeApp(firebaseConfig);
 
