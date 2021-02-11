29cc74a9215b817570d9888dff6859096d5aae83
Chris Wan <chriswan.wan@mail.utoronto.ca>
2021-02-10 21:14:00 -0500

    [CHED-3] Finish GET API request for resume templates

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 const firebase = require("firebase");
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+const port = 8000;
 
 const firebaseConfig = {
     apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
@@ -13,7 +19,34 @@ var fire_app = firebase.initializeApp(firebaseConfig);
 
 const db = fire_app.firestore();
 
+app.get("/Templates", async (req, res) => {
 
+    try {
+        let collection = req.body.collection
+        let document = req.body.document
+
+        let templates = []
+
+        let snapShot = await db.collection("Templates").doc(`${document}`).collection(`${collection}`).get()
+
+        snapShot.forEach(doc => {
+
+            let id = doc.id
+            let data = doc.data()
+            templates.push({ id, ...data })
+
+        })
+
+        res.status(200).send(JSON.stringify(templates));
+    }
+    catch (error) {
+        res.status(400).send("Bad Request")
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
 
 
