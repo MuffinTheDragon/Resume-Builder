af5b01fe9a9ddadba119fc198f559e10b10a6146
RendraChan <mrendrachan@gmail.com>
2021-02-19 16:23:38 -0500

    [CHED-41] Starting Code Template

---
+++ frontend/src/components/JobExperienceResume/JobExperienceResume.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Personal.module.css";
+import shared from "../Shared.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+function JobExperienceResume() {
+    return (
+        <div class={shared.editmenu}>
+            putting personal section here 
+        </div>
+    );
+}
+
+export default JobExperienceResume;
