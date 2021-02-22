42f56abcdf881201401103cbc044cb28f4d9d606
Dhaval <dhaval.malhotra@mail.utoronto.ca>
2021-02-22 15:17:17 -0500

    [CHED-40] Change Achievements Render to use map

--- frontend/src/components/Achievements/AchievementsRender.js
+++ frontend/src/components/Achievements/AchievementsRender.js
@@ -3,22 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import './style.css'
 
 function AchievementsRender(props) {
-
-    var arr = [];
     
-    for (var i=0;i<props.count;i++) {
-        arr.push (
+    const titles = props.title;
+    const arr = titles.map((title, index) => {
+        return (
             <div class="block">
                 <div class="trophy"><FontAwesomeIcon icon={faTrophy} /></div>
                 <div>
-                    <h4 class="title">{props.title[i]}</h4>
-                    <h4 class="desc">{props.desc[i]}</h4>
+                    <h4 class="title">{title}</h4>
+                    <h4 class="desc">{props.desc[index]}</h4>
                 </div>
             </div>
-            
-        )
-    }
-
+        
+            );
+        })
     return arr;
 }
 
