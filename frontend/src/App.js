93bf99121858dd527f772acaeea4245b6624c0ac
Chris Lim <hello@hiimchrislim.co>
2021-02-22 18:33:33 -0500

    Revert "[CHED-40] Implement achievements and education components"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,6 @@ import Experience from "./components/Experience/Experience"
 import Additional from "./components/Additional/Additional";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EducationHistory from "./components/EducationHistory/EducationHistory"
-import EducationHistoryRender from "./components/EducationHistory/EducationHistoryRender"
-import AchievementsRender from "./components/Achievements/AchievementsRender"
 
 function App() {
     return (
@@ -16,9 +14,6 @@ function App() {
             <EducationHistory />
             <Experience />
             <Additional />
-            <EducationHistoryRender school="University of Toronto" degree="H.B.Sc Computer Science & Statistics" date="Sept 2015 - June 2020" gpa="3.7"/>
-            <hr></hr>
-            <AchievementsRender title={['Dean\'s List (2018-2020)', 'Honorable Mention (Top 20)']} desc={['GPA of 3.50 or above', 'Adobe Creative Jams, 3 times']} />
         </div>
     );
 }
