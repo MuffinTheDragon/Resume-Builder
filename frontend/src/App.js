9139d67fae1f833e1232e538c2c63c4b8c506ac1
Chris <hello@hiimchrislim.co>
2021-02-24 19:24:39 -0500

    [CHED-40] Complete Resume Collection Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import EducationHistory from "./components/EducationHistory/EducationHistory"
 import EducationHistoryRender from "./components/EducationHistory/EducationHistoryRender"
 import AchievementsRender from "./components/Achievements/AchievementsRender"
+import Collection from "./components/Collection/Collection";
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
             <EducationHistoryRender school="University of Toronto" degree="H.B.Sc Computer Science & Statistics" date="Sept 2015 - June 2020" gpa="3.7"/>
             <hr></hr>
             <AchievementsRender title={['Dean\'s List (2018-2020)', 'Honorable Mention (Top 20)']} desc={['GPA of 3.50 or above', 'Adobe Creative Jams, 3 times']} />
+            <Collection items={["Java", "Python", "MySQL", "NoSQL", "REST", "Spring", "JavaScript", "Vue", "React", "Node", "Express", "HTML", "CSS", "Bootstrap", "Firebase", "Android", "Git", "Unix", "Agile", "Scrum", "SDLC", "Jira"]}/>
         </div>
     );
 }
