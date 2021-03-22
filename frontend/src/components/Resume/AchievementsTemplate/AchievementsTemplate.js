912cffbfdc72d29276ea5b857eb2c732bd2cc352
Dhaval <dhaval.malhotra@mail.utoronto.ca>
2021-03-22 12:57:22 -0400

    [CHED-2] Implement export resume to PDF

--- frontend/src/components/Resume/AchievementsTemplate/AchievementsTemplate.js
+++ frontend/src/components/Resume/AchievementsTemplate/AchievementsTemplate.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import TemplateStyles from "../TemplateStyles.module.css";
 import styles from "./AchievementsTemplate.module.css";
 import { faTrophy } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AddToLibrary } from "../Resume";
 
 function AchievementsTemplate({title, desc}) {
+    // useContext(AddToLibrary(faTrophy));
+    AddToLibrary(faTrophy);
     return (
         <div className="m-2">
             <div className={styles.trophy}>
-            {(title === "" && desc === "") ? <></> : <FontAwesomeIcon icon={faTrophy}/>}
+            {(title === "" && desc === "") ? <></> : <FontAwesomeIcon icon={faTrophy} width={200} height={200}/>}
             </div>
             <div>
                 <h4>{title}</h4>
--- frontend/src/components/Resume/HackathonTemplate/HackathonTemplate.js
+++ frontend/src/components/Resume/HackathonTemplate/HackathonTemplate.js
@@ -2,8 +2,10 @@ import React from "react";
 import styles from '../TemplateStyles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { AddToLibrary } from "../Resume";
 
 function HackathonTemplate(props) {
+    AddToLibrary(faCalendarAlt);
     const listItems = props.desc.map((point, index) => {
         return (
             <li key={index}>
@@ -17,7 +19,7 @@ function HackathonTemplate(props) {
         <div>
             <h2>{props.title}</h2>
             <h3 className="resumeRender">{props.subtitle}</h3>
-            <span className={styles.date}>{props.startDate !== "" ? <><FontAwesomeIcon icon={faCalendarAlt} /> {props.startDate} - {props.endDate}</> : ''}</span>
+            <span className={styles.date}>{props.startDate !== "" ? <><FontAwesomeIcon icon={faCalendarAlt} width={200} height={200}/> {props.startDate} - {props.endDate}</> : ''}</span>
             {(props.desc[0] === "" && props.desc.length === 1) ? "" : <ul>{listItems}</ul>}
         </div>
     );
